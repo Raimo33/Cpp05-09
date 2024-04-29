40c0f27479b5d292cfbca0509554d6c5321aaeed
Claudio Raimondi <craimond@c1r4p2.42firenze.it>
2024-04-29 14:42:05 +0200

    Add ex02

---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 14:14:46 by craimond          #+#    #+#             */
+/*   Updated: 2024/04/29 14:41:43 by craimond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+#include <iostream>
+#include <cstdlib> //for rand() and srand()
+
+void identify(Base* p);
+void identify(Base& p);
+Base *generate(void);
+
+int main(void)
+{
+	Base *obj;
+
+	srand(time(NULL));
+	obj = generate();
+	identify(obj);
+	identify(*obj);
+	delete obj;
+}
+
+Base *generate(void)
+{
+	switch (rand() % 3)
+	{
+		case 0:
+			return new A;
+		case 1:
+			return new B;
+		case 2:
+			return new C;
+	}
+	return NULL;
+}
+
+void identify(Base* p)
+{
+	std::cout << "Object type: ";
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "Unknown" << std::endl;
+}
+
+void identify(Base& p)
+{
+	identify(&p);
+}
